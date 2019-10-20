9fb7597a54df68ea3af7921440b4c2ebe0163352
Handi Hermawan <handihand53@gmail.com>
2019-10-20 16:16:14 +0700

    edit login method

--- src/main/java/com/parking/center/parkingcenter/Database.java
+++ src/main/java/com/parking/center/parkingcenter/Database.java
@@ -18,14 +18,30 @@ import java.sql.Statement;
 public class Database {
     private  Connection conn = null;
     
-    public void connect() {
+    public void connect(String username, String password) {
        
         try {
             // db parameters
             String url = "jdbc:sqlite:database\\parkingCenter.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
-            
+            Statement stmt = conn.createStatement();
+            System.out.println("1");
+            System.out.println("2");
+        
+            System.out.println(username);
+            System.out.println(password);
+            String query= "SELECT * FROM user WHERE username = '"+username+"' and password='"+password+"'";
+            System.out.println(query);
+
+            ResultSet rs = stmt.executeQuery(query);
+            System.out.println("3");
+            if(rs.next()) {
+                System.out.println("ada");
+            }else{
+                System.out.println("tidak ada");
+            }
+            conn.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -40,13 +56,7 @@ public class Database {
     }
     
     public void userQuery(String username, String password) throws SQLException{
-        connect();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT username FROM user WHERE username "+username+" and password="+password+"");
-        if(rs.next()) {
-            System.out.println("ada");
-        }else{
-            System.out.println("tidak ada");
-        }
+//        connect();
+        
     }
 }
