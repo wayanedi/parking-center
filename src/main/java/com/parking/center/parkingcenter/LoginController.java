0cf09ab92bba458a007d14d34d5a9d3bf983b2b9
Handi Hermawan <handihand53@gmail.com>
2019-10-20 12:29:10 +0700

    login fxml

---
+++ src/main/java/com/parking/center/parkingcenter/LoginController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.parking.center.parkingcenter;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+/**
+ * FXML Controller class
+ *
+ * @author asus
+ */
+public class LoginController implements Initializable {
+    
+    @FXML
+    private void loginButton(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
