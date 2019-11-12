06af5591a5a5f41ac1592fc70c0b692e38640d3d
Handi Hermawan <handihand53@gmail.com>
2019-11-12 22:39:11 +0700

    adding sql injection checker in jenis kendaraan and laporan

--- src/main/java/com/parking/center/parkingcenter/DB/LaporanDAO.java
+++ src/main/java/com/parking/center/parkingcenter/DB/LaporanDAO.java
@@ -8,6 +8,7 @@ package com.parking.center.parkingcenter.DB;
 import com.parking.center.parkingcenter.model.CatatKeluarModel;
 import com.parking.center.parkingcenter.model.LaporanModel;
 import com.parking.center.parkingcenter.model.SisaSlotModel;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javafx.collections.FXCollections;
@@ -24,16 +25,20 @@ public class LaporanDAO {
         
         String query;
         CatatKeluarModel catat = null;
-//        query = "SELECT laporan.waktu_masuk, jenis_kendaraan.nama_kendaraan, jenis_kendaraan.harga_perhari, jenis_kendaraan.harga_perjam, jenis_kendaraan.harga_set_hari from laporan INNER JOIN jenis_kendaraan on laporan.jenis_kendaraan = jenis_kendaraan.id_jenis_kendaraan where plat_nomor='"+plat+"' AND status_kendaraan='0'";
-//        System.out.println(query);
-//         
-//        ResultSet rs = DBUtil.getInstance().dbExecuteQuery(query);
-//        System.out.println("2");
-//        if(rs.next()){
-//            System.out.println("ada datanya");
-//            catat = new CatatKeluarModel(rs.getString("waktu_masuk"), rs.getInt("harga_perjam"), rs.getInt("harga_set_hari"), rs.getInt("harga_perhari"), rs.getString("nama_kendaraan"));
-//            
-//        }
+        query = "SELECT laporan.waktu_masuk, jenis_kendaraan.nama_kendaraan, jenis_kendaraan.harga_perhari, jenis_kendaraan.harga_perjam, jenis_kendaraan.harga_set_hari from laporan INNER JOIN jenis_kendaraan on laporan.jenis_kendaraan = jenis_kendaraan.id_jenis_kendaraan where plat_nomor=? AND status_kendaraan='0'";
+        System.out.println(query);
+         
+        DBUtil db = DBUtil.getInstance();
+        db.dbConnect();
+        PreparedStatement preparedStatement;
+        preparedStatement = db.conn.prepareStatement(query);
+        preparedStatement.setString(1, plat);
+        ResultSet rs = db.dbExecuteQuery(preparedStatement);
+        if(rs.next()){
+            System.out.println("ada datanya");
+            catat = new CatatKeluarModel(rs.getString("waktu_masuk"), rs.getInt("harga_perjam"), rs.getInt("harga_set_hari"), rs.getInt("harga_perhari"), rs.getString("nama_kendaraan"));
+            
+        }
 
         return catat;
         
@@ -41,8 +46,14 @@ public class LaporanDAO {
     
     public static void updateLaporan(CatatKeluarModel catat) throws SQLException, ClassNotFoundException{
         
-        String query = "UPDATE laporan SET total_harga='"+catat.getTotalHarga()+"', waktu_keluar='"+catat.getWaktuKeluar()+"', status_kendaraan='"+1+"'";
-        DBUtil.getInstance().dbExecuteUpdate(query);
+        String query = "UPDATE laporan SET total_harga=?, waktu_keluar=?, status_kendaraan='"+1+"'";
+        DBUtil db = DBUtil.getInstance();
+        db.dbConnect();
+        PreparedStatement preparedStatement;
+        preparedStatement = db.conn.prepareStatement(query);
+        preparedStatement.setInt(1, catat.getTotalHarga());
+        preparedStatement.setString(2, catat.getWaktuKeluar());
+        ResultSet rs = db.dbExecuteQuery(preparedStatement);
         
     }
     
@@ -51,9 +62,18 @@ public class LaporanDAO {
         String query;
         try {
         query = "INSERT INTO laporan (plat_nomor, id_petugas, jenis_kendaraan, waktu_masuk, status_kendaraan)"
-                + "VALUES('"+laporan.getPlatNomor()+"', '"+id+"', '"+laporan.getJenisKendaraan()+"', '"+laporan.getWaktuMasuk()+"', '"+0+"')";
+                + "VALUES(?, ?, ?, ?, '"+0+"')";
                System.out.println(query);
-        DBUtil.getInstance().dbExecuteUpdate(query);  
+            DBUtil db = DBUtil.getInstance();
+            db.dbConnect();
+            PreparedStatement preparedStatement;
+            preparedStatement = db.conn.prepareStatement(query);
+            preparedStatement.setString(1, laporan.getPlatNomor());
+            preparedStatement.setInt(2, id);
+            preparedStatement.setString(3, laporan.getJenisKendaraan());
+            preparedStatement.setString(4, laporan.getWaktuMasuk());
+            
+            ResultSet rs = db.dbExecuteQuery(preparedStatement);  
             Alert alert;
             alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Catat Masuk");   
@@ -79,12 +99,15 @@ public class LaporanDAO {
         query = "SELECT nama_kendaraan, id_jenis_kendaraan, slot from jenis_kendaraan";
         System.out.println(query);
          
-//        ResultSet rs = DBUtil.getInstance().dbExecuteQuery(query);
-//        
-//        ObservableList<SisaSlotModel> sisa = getListSlot(rs);
-//        
-//        return sisa;
-return  null;
+        DBUtil db = DBUtil.getInstance();
+        db.dbConnect();
+        PreparedStatement preparedStatement;
+        preparedStatement = db.conn.prepareStatement(query);
+        ResultSet rs = db.dbExecuteQuery(preparedStatement);
+        
+        ObservableList<SisaSlotModel> sisa = getListSlot(rs);
+        
+        return sisa;
         
     }
     
@@ -105,12 +128,17 @@ return  null;
         query2 = "SELECT count(jenis_kendaraan) as slot from laporan where status_kendaraan='0' group by jenis_kendaraan";
         System.out.println(query2);
         int count =0;
-//        ResultSet rs2 = DBUtil.getInstance().dbExecuteQuery(query2);
-//        while(rs2.next()){
-//            System.out.println("");
-//            sisa.get(count).setSlot(sisa.get(count).getSlot()-rs2.getInt("slot"));
-//            count++;
-//        }
+        DBUtil db = DBUtil.getInstance();
+            db.dbConnect();
+        PreparedStatement preparedStatement;
+        preparedStatement = db.conn.prepareStatement(query2);
+
+        ResultSet rs2 = db.dbExecuteQuery(preparedStatement);
+        while(rs2.next()){
+            System.out.println("");
+            sisa.get(count).setSlot(sisa.get(count).getSlot()-rs2.getInt("slot"));
+            count++;
+        }
         
         return sisa;
         
@@ -119,11 +147,16 @@ return  null;
     public static boolean checkPlat(String plat) throws SQLException, ClassNotFoundException{
         String query = "SELECT * from laporan where plat_nomor='"+plat+"' AND status_kendaraan='0'";
          
-//        ResultSet rs = DBUtil.getInstance().dbExecuteQuery(query);
-//        
-//        if(rs.next()){
-//           return true;  
-//        }
+        DBUtil db = DBUtil.getInstance();
+        db.dbConnect();
+        PreparedStatement preparedStatement;
+        preparedStatement = db.conn.prepareStatement(query);
+        preparedStatement.setString(1, plat);
+        ResultSet rs = db.dbExecuteQuery(preparedStatement);
+        
+        if(rs.next()){
+           return true;  
+        }
         
         return false;
     }
